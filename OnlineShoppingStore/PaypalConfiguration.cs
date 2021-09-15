f04f87b69ce9d1655fbd7775ba1718b62bbe2c25
ahmedheroo <69500751+ahmedheroo@users.noreply.github.com>
2021-09-15 10:23:33 +0200

    First load

---
+++ OnlineShoppingStore/PaypalConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using PayPal.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShoppingStore
+{
+    public class PaypalConfiguration
+    {
+        public readonly static string clientId;
+        public readonly static string clientSecret;
+
+
+        static PaypalConfiguration()
+        {
+            var config = getconfig();
+            clientId = "AVJpGRZCpVJEEMcTNlnVLGi4ddjuC83oo1UwAMJeBkoLRXCu-hcm7jY71P3FrBug7L4V4mvn08Tnrv57";
+            clientSecret = "EOEdfm-OxIGXkGqEA5a1hN1bgMMXrdjAb3RxCUuMCktJJxRnUm6IRXBECPlovkdT3ZBF6-SHQh3hwGJ6";
+        }
+
+        private static Dictionary<string, string> getconfig()
+        {
+            return PayPal.Api.ConfigManager.Instance.GetProperties();
+        }
+
+        private static string GetAccessToken()
+        {
+            string accessToken = new OAuthTokenCredential(clientId, clientSecret, getconfig()).GetAccessToken();
+            return accessToken;
+        }
+        public static APIContext GetAPIContext()
+        {
+            APIContext apicontext = new APIContext(GetAccessToken());
+            apicontext.Config = getconfig();
+            return apicontext;
+        }
+    }
+}
